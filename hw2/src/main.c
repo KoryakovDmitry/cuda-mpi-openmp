bf8398607d169c178ce15a74d1689be4603f1bd7
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2024-11-09 03:29:21 +0100

    minor fix

--- hw2/src/main.c
+++ hw2/src/main.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 void bub_sort(float *arr, int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 float temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -23,13 +24,14 @@ int main() {
         return 1;
     }
 
-    for (int i = 0; i < n; i++) {
+    int i;
+    for (i = 0; i < n; i++) {
         scanf("%f", &arr[i]);
     }
 
     bub_sort(arr, n);
 
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         printf("%.6e ", arr[i]);
     }
     printf("\n");
