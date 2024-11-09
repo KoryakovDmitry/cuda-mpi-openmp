b9ef19a813a70067a8c9398e5c690f4b0eacf2b7
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2024-11-09 22:56:43 +0100

    init learn_cuda.cu

---
+++ lab1/src/learn_cuda.cu
@@ -0,0 +1,21 @@
+//
+// Created by dm on 09/11/2024.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <cudaruntime.h>
+
+int main() {
+    int i, n = 10000;
+    int *arr = (int *)malloc(sizeof(int) * n);
+    for (i = 0; i < n; i++) {
+        arr[i] = i;
+    }
+
+    int *dev_arr;
+    cudaMalloc(&dev_arr, sizeof(int) * n);
+
+
+    free(arr);
+    return 0;
+}
