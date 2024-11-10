f920c95358506749408ddce4a92b446e6fe1012d
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2024-11-10 06:15:58 +0100

    minor fix

--- lab1/src/main.cu
+++ lab1/src/main.cu
@@ -2,21 +2,20 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 
-
 #define CSC(call)                                               \
     do {                                                        \
         cudaError_t status = call;                              \
-    if (status != cudaSuccess) {                                \
-        fprintf(stderr, "[ERROR CUDA] File: '%s'; Line: %i; Message: %s.\n", \
-                __FILE__, __LINE__, cudaGetErrorString(err));   \
-        exit(1);                                                \
-    }                                                           \
+        if (status != cudaSuccess) {                            \
+            fprintf(stderr, "[ERROR CUDA] File: '%s'; Line: %i; Message: %s.\n", \
+                    __FILE__, __LINE__, cudaGetErrorString(status));   \
+            exit(1);                                            \
+        }                                                       \
     } while (0)
 
 #define ALLOCATE_VECTOR_CPU(host_arr, n)                        \
     double *host_arr = (double *)malloc(n * sizeof(double));    \
     if (host_arr == NULL) {                                     \
-        fprintf(stderr, "[ERROR CPU] File: '%s'; Line: %i; Error in allocating mem for vector: `%s`\n", __FILE__, __LINE__, #arr); \
+        fprintf(stderr, "[ERROR CPU] File: '%s'; Line: %i; Error in allocating mem for vector: `%s`\n", __FILE__, __LINE__, #host_arr); \
         return 1;                                               \
     }
 
@@ -35,14 +34,14 @@ int main() {
 
     // set dim of vector
     scanf("%d", &n);
-    
+
     // set mem for first vector on CPU
     ALLOCATE_VECTOR_CPU(host_arr_1, n);
     // set mem for second vector on CPU
     ALLOCATE_VECTOR_CPU(host_arr_2, n);
     // set mem for result vector on CPU
     ALLOCATE_VECTOR_CPU(host_arr_3, n);
-    
+
     // fill first vector
     for (i = 0; i < n; i++) {
         scanf("%lf", &host_arr_1[i]);
@@ -58,18 +57,17 @@ int main() {
     CSC(cudaMalloc(&dev_arr_2, sizeof(double) * n));
     CSC(cudaMalloc(&dev_arr_3, sizeof(double) * n));
 
-    CSC(cudaMemcpy(dev_arr_1, host_arr_1, sizeof(double) * n), cudaMemcpyHostToDevice);
-    CSC(cudaMemcpy(dev_arr_2, host_arr_2, sizeof(double) * n), cudaMemcpyHostToDevice);
+    CSC(cudaMemcpy(dev_arr_1, host_arr_1, sizeof(double) * n, cudaMemcpyHostToDevice));
+    CSC(cudaMemcpy(dev_arr_2, host_arr_2, sizeof(double) * n, cudaMemcpyHostToDevice));
 
     cudaEvent_t start, stop;
     CSC(cudaEventCreate(&start));
     CSC(cudaEventCreate(&stop));
 
     CSC(cudaEventRecord(start));
-    kernel <<< 512, 512 >>> (dev_arr_1, dev_arr_2, dev_arr_3, n);
+    kernel<<<512, 512>>>(dev_arr_1, dev_arr_2, dev_arr_3, n);
     CSC(cudaEventRecord(stop));
     CSC(cudaEventSynchronize(stop));
-    CSC(cudaEventSynchronize());
     CSC(cudaGetLastError());
 
     float t;
@@ -79,13 +77,12 @@ int main() {
 
     printf("CUDA execution time: %f ms\n", t);
 
-    CSC(cudaMemcpy(host_arr_3, dev_arr_3, sizeof(double) * n), cudaMemcpyDeviceToHost);
+    CSC(cudaMemcpy(host_arr_3, dev_arr_3, sizeof(double) * n, cudaMemcpyDeviceToHost));
 
     for (i = 0; i < n; i++) {
         printf("%.10e ", host_arr_3[i]);
     }
     printf("\n");
-
 
     CSC(cudaFree(dev_arr_1));
     CSC(cudaFree(dev_arr_2));
