e290aa50f61e6980ebf56e7f9d3c5c18ef2f5c48
DmitryKoryakov <dmitry.koryakov@initflow.com>
2024-11-11 02:51:52 +0100

    added stat scripts

--- lab1/test.py
+++ lab1/test.py
@@ -1,13 +1,15 @@
 import asyncio
 import json
 import os
+import re
 import shutil
 import subprocess
+import sys
 import time
+from typing import Tuple, List, Optional
 
 import numpy as np
-import sys
-from typing import Tuple, List
+import pandas as pd
 
 np.random.seed(42)
 
@@ -87,14 +89,17 @@ async def run_subprocess(
         return False, None, None, err, None
 
 
-async def run_binary(
+async def run_kernel(
     binary_path: str,
     k_times: int,
     max_vector_size: int,
-    kernel_size_1: int = None,
-    kernel_size_2: int = None,
-):
+    kernel_sizes: Optional[List[Tuple[int, int]]] = None,
+) -> Optional[pd.DataFrame]:
+    df_scores = None
     tasks = []
+    if kernel_sizes is None:
+        kernel_sizes = [[None, None]]
+
     for i in range(k_times):
         n = np.random.randint(1, max_vector_size)
 
@@ -103,31 +108,33 @@ async def run_binary(
         second_vector = np.random.uniform(double_left, double_right, n)
 
         print(f"[{i}] started")
-        tasks.append(
-            {
-                "idx": i,
-                "time_st": time.time(),
-                "time_exe_ms_full": None,
-                "time_exe_ms_calc": None,
-                "task": asyncio.create_task(
-                    run_subprocess(
-                        binary_path=binary_path,
-                        n=n,
-                        first_vector=first_vector,
-                        second_vector=second_vector,
-                        kernel_size_1=kernel_size_1,
-                        kernel_size_2=kernel_size_2,
-                    )
-                ),
-                "n": n,
-                "first_vector": first_vector,
-                "second_vector": second_vector,
-                "status": None,
-                "result_vector": None,
-                "test_result": None,
-                "err": None,
-            }
-        )
+        for kernel_size_1, kernel_size_2 in kernel_sizes:
+            tasks.append(
+                {
+                    "idx": i,
+                    "time_st": time.time(),
+                    "time_exe_ms_full": None,
+                    "time_exe_ms_calc": None,
+                    "task": asyncio.create_task(
+                        run_subprocess(
+                            binary_path=binary_path,
+                            n=n,
+                            first_vector=first_vector,
+                            second_vector=second_vector,
+                            kernel_size_1=kernel_size_1,
+                            kernel_size_2=kernel_size_2,
+                        )
+                    ),
+                    "n": n,
+                    "first_vector": first_vector,
+                    "second_vector": second_vector,
+                    "status": None,
+                    "result_vector": None,
+                    "test_result": None,
+                    "err": None,
+                    "kernel_size": [kernel_size_1, kernel_size_2],
+                }
+            )
 
     for i in range(k_times):
         result = await tasks[i]["task"]
@@ -142,62 +149,100 @@ async def run_binary(
         )
 
     await get_stat_time([item.get("time_exe_ms_calc") for item in tasks])
+    bin_name = os.path.splitext(os.path.basename(binary_path))[0]
 
     if all(item.get("test_result") for item in tasks):
+
+        df_scores = pd.DataFrame(
+            [
+                {
+                    "idx": item.get("idx"),
+                    "time_exe_ms_full": item.get("time_exe_ms_full"),
+                    "time_exe_ms_calc": item.get("time_exe_ms_calc"),
+                    "n": item.get("n"),
+                    # "first_vector": item.get("first_vector"),
+                    # "second_vector": item.get("second_vector"),
+                    "status": item.get("status"),
+                    # "result_vector": item.get("result_vector"),
+                    "test_result": item.get("test_result"),
+                    "err": item.get("err"),
+                    "kernel_size": item.get("kernel_size"),
+                }
+                for item in tasks
+            ]
+        )
+        df_scores.to_csv(f"stat_conditions_{bin_name}.csv", index=False)
         print("SUCCESS")
     else:
         failed = [item for item in tasks if not item.get("test_result")]
         print(f"FAILED: len={len(failed)}")
 
-        dir2save = "./failed"
+        dir2save = f"./failed_{bin_name}"
         os.makedirs(dir2save, exist_ok=True)
         shutil.rmtree(dir2save)
         os.makedirs(dir2save, exist_ok=True)
         for fail in failed:
             with open(
-                os.path.join(dir2save, f'{fail["idx"]}_first_vector.npy'), "wb"
+                os.path.join(dir2save, f'{fail["idx"]}_first_vector_{bin_name}.npy'),
+                "wb",
             ) as f:
                 np.save(f, fail["first_vector"])
 
             with open(
-                os.path.join(dir2save, f'{fail["idx"]}_second_vector.npy'), "wb"
+                os.path.join(dir2save, f'{fail["idx"]}_second_vector_{bin_name}.npy'),
+                "wb",
             ) as f:
                 np.save(f, fail["second_vector"])
 
             with open(
-                os.path.join(dir2save, f'{fail["idx"]}_result_vector.npy'), "wb"
+                os.path.join(dir2save, f'{fail["idx"]}_result_vector_{bin_name}.npy'),
+                "wb",
             ) as f:
                 np.save(f, fail["result_vector"])
 
-        with open("failed.json", "w", encoding="utf8") as f:
-            json.dump(
-                [
-                    {
-                        "idx": fail.get("idx"),
-                        "time_exe_ms_full": fail.get("time_exe_ms_full"),
-                        "time_exe_ms_calc": fail.get("time_exe_ms_calc"),
-                        "n": fail.get("n"),
-                        # "first_vector": fail.get("first_vector"),
-                        # "second_vector": fail.get("second_vector"),
-                        "status": fail.get("status"),
-                        # "result_vector": fail.get("result_vector"),
-                        "test_result": fail.get("test_result"),
-                        "err": fail.get("err"),
-                    }
-                    for fail in failed
-                ],
-                f,
-                ensure_ascii=False,
-                indent=2,
-            )
+        df_failed = pd.DataFrame(
+            [
+                {
+                    "idx": fail.get("idx"),
+                    "time_exe_ms_full": fail.get("time_exe_ms_full"),
+                    "time_exe_ms_calc": fail.get("time_exe_ms_calc"),
+                    "n": fail.get("n"),
+                    # "first_vector": fail.get("first_vector"),
+                    # "second_vector": fail.get("second_vector"),
+                    "status": fail.get("status"),
+                    # "result_vector": fail.get("result_vector"),
+                    "test_result": fail.get("test_result"),
+                    "err": fail.get("err"),
+                    "kernel_size": fail.get("kernel_size"),
+                }
+                for fail in failed
+            ]
+        )
+        df_failed.to_csv(f"failed_{bin_name}.csv", index=False)
+    return df_scores
+
+
+async def run(
+    binary_path: str,
+    k_times: int,
+    max_vector_size: int,
+    kernel_sizes: Optional[List[Tuple[int, int]]] = None,
+):
+    df_scores = await run_kernel(
+        binary_path=binary_path,
+        k_times=k_times,
+        max_vector_size=max_vector_size,
+        kernel_sizes=kernel_sizes
+    )
+    return df_scores
 
 
 if __name__ == "__main__":
-    kernel_size_1 = None
-    kernel_size_2 = None
+    kernel_sizes = None
     if len(sys.argv) != 4:
         print(
-            "Usage: python script.py <binary_path> <k_times> <max_vector_size> [<kernel_size_1>] [<kernel_size_2>]"
+            # "Usage: python script.py <binary_path> <k_times> <max_vector_size> [<kernel_size_1>] [<kernel_size_2>]"
+            "Usage: python script.py <binary_path> <k_times> <max_vector_size> [<kernel_sizes> ([[1, 32], [512, 512], [1024, 1024]])]"
         )
         # sys.exit(1)
         binary_path = "lab1/src/a.out"
@@ -208,10 +253,7 @@ if __name__ == "__main__":
         binary_path = sys.argv[1]
         k_times = int(sys.argv[2])
         max_vector_size = int(sys.argv[3])
-        if os.path.splitext(binary_path)[1] == ".cu":
-            kernel_size_1 = int(sys.argv[4])
-            kernel_size_2 = int(sys.argv[5])
+        if "cuda" in os.path.basename(binary_path):
+            kernel_sizes = json.loads(str(sys.argv[4]))
 
-    asyncio.run(
-        run_binary(binary_path, k_times, max_vector_size, kernel_size_1, kernel_size_2)
-    )
+    df_scores = asyncio.run(run(binary_path, k_times, max_vector_size, kernel_sizes))
